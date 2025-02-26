396d25b8c6894472988d86f6dd9d4212a9301b1f
Kashyap Patel <ksnakrani5544@gmail.com>
2025-02-27 00:48:38 +0530

    feat: add firbase connection

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -1,13 +1,32 @@
-import { currentUser } from "@clerk/nextjs/dist/types/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { db } from "../../../../config/firebaseConfig";
+import { doc, setDoc } from "firebase/firestore";
 
+export async function POST() {
+  const user = await currentUser();
 
-export async function GET(req: Request) {
-   const user = await currentUser();
+  if (!user) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
 
-    console.log(user.id);
-    console.log(user.emailAddresses);
-    console.log(user.firstName);
-    console.log(user);
-   
+  // Extract the first email as a string
+  const userEmail = user.emailAddresses?.[0]?.emailAddress || "";
+
+  const userRef = doc(db, "users", user.id);
+
+  const data = await setDoc(userRef, {
+    userId: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: userEmail,
+    profileUrl: user.imageUrl,
+    role: "ADMIN",
+  });
+  console.log(data);
+
+  return Response.json(
+    { message: "User created in Firestore" },
+    { status: 200 }
+  );
 }
-
--- models/user.ts
+++ models/user.ts
@@ -1,8 +1,8 @@
 export interface UserModel {
-    id?: string; // Firebase will auto-generate this when adding a document
-    name: string;
-    email: string;
-    message: string;
-    imageUrl: string;
-    cartItems: object;
-  }
+  id?: string; // Firebase will auto-generate this when adding a document
+  name: string;
+  email: string;
+  message: string;
+  imageUrl: string;
+  cartItems: object;
+}
